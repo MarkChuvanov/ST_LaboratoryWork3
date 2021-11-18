211c94882a70b54fdde03a9ba4619f9d78c1e0e5
MarkChuvanov <markchuvanov@mail.ru>
2021-11-18 09:52:10 +0300

    Create new tests

--- ST_LaboratoryWork3/VersionTest.cs
+++ ST_LaboratoryWork3/VersionTest.cs
@@ -17,11 +17,26 @@ namespace ST_LaboratoryWork3
 			{ 
 				bool metka = new Version("1.0,0-beta.alpha") > new Version("1.0.0-beta.1");
 			});
+			Assert.Throws<ArgumentException>(() =>
+			{
+				bool metka = new Version("100") > new Version("1.0.0-beta.1");
+			});
 			Assert.IsTrue(new Version("1.0.0-beta.alpha") > new Version("1.0.0-beta.1"));
 			Assert.IsTrue(new Version("1.0.0") > new Version("1.0.0-alpha"));
 			Assert.IsTrue(new Version("1.0.1") > new Version("1.0.0"));
 			Assert.IsTrue(new Version("1.1.0") > new Version("1.0.0"));
 			Assert.IsTrue(new Version("2.0.0") > new Version("1.0.0"));
+		}
+
+		[Test]
+		public void LessTest ()
+		{
+			Assert.Throws<ArgumentException>(() =>
+			{
+				bool metka = new Version("%.0,0-beta.alpha") > new Version("1.0.0-beta.1");
+			});
+			Assert.IsTrue(new Version("1.0.0-beta.1") < new Version("1.0.0-beta.alpha"));
+			Assert.IsFalse(new Version("1.0.0") < new Version("1.0.0-alpha"));
 		}
 
 		[Test]
@@ -47,6 +62,17 @@ namespace ST_LaboratoryWork3
 			Assert.IsTrue(new Version("1.0.0") == new Version("1.0.0"));
 			Assert.IsTrue(new Version("1.0.0") == new Version("1.0.0"));
 			Assert.IsTrue(new Version("1.1.1-alpha") == new Version("1.1.1-alpha"));
+		}
+
+		[Test]
+		public void NoEqualTest ()
+		{
+			Assert.Throws<ArgumentException>(() =>
+			{
+				bool metka = new Version("1.0,0-beta.alpha") > new Version("ABC");
+			});
+			Assert.IsTrue(new Version("1.0.0") != new Version("1.0.1"));
+			Assert.IsFalse(new Version("1.0.0") != new Version("1.0.0"));
 		}
 	}
 }
